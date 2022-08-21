00eebca914af3543cf3066efb04cedc495161fa1
McKB <mckennakb@comcast.net>
2022-08-21 16:36:25 -0400

    background and colors and some spacing

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
                 <h3>creations by</h3>
                 <h1>mcKenna k black</h1>
             </header>
-            <div className='textBlock'>
+            <div className='textBlock descriptors'>
                 <TextBlock message={['web developer', 'hiker', 'beagle-owner', 'vermonter', 'skier', 'bookworm', 'loner']} />
             </div>
             <div className="projectsContainer">
@@ -24,8 +24,8 @@ const Main = () => {
                     <a href='source/code'>fakesourcecode</a>
                 </div>
             </div>
-            <div className='textBlock'>
-                <TextBlock message={['"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"']}/>
+            <div className='textBlock bio'>
+                <TextBlock message={['I’m a web developer & designer open to collaborating on new projects and expanding my skillset, which currently includes HTML, CSS, Javascript, React, SQL, Github, Git, Github Actions, CI/CD, Agile Software Development, & Adobe Illustrator/Photoshop. I can also tell you where to hike in northern Vermont, spell two words that are over 25 letters long, and communicate effectively. Let’s chat >>']}/>
             </div>
             <div className="contactMeContainer">
                 <div>Contact me --</div>
