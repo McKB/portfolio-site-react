aede0a6386d765caa05ceb3a90c9d1c39e911ab1
McKB <mckennakb@comcast.net>
2022-08-29 19:46:05 -0400

    transform profiles

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -37,12 +37,10 @@ const Main = () => {
                     <TextBlock message={['I’m a web developer & designer open to collaborating on new projects and expanding my skillset, which currently includes HTML, CSS, Javascript, React, SQL, Github, Git, Github Actions, CI/CD, Agile Software Development, & Adobe Illustrator/Photoshop. I can also tell you where to hike in northern Vermont, spell two words that are over 25 letters long, and communicate effectively. Let’s chat >>']}/>
                 </div>
                 <div className="contactMeContainer">
-                    <ul>
-                        <li id="github"> <a href="https://github.com/McKB" target="_blank" rel="noreferrer"><img src={github} alt="Github"></img></a></li>
-                        <li id="linkedin"> <a href="https://www.linkedin.com/in/mckennakblack" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn"></img></a></li>
-                        <li id="instagram"> <a href="https://www.instagram.com/mckennakblack/" target="_blank" rel="noreferrer"><img src={instagram} alt="Instagram"></img></a></li>
-                        <li id="email"> <a href="mailto:mckennakb@comcast.net?subject=Hello!"><img src={email} alt="Email Me"></img></a></li>
-                    </ul>
+                    <div className='profile' id="github"> <a href="https://github.com/McKB" target="_blank" rel="noreferrer"><img src={github} alt="Github"></img></a></div>
+                    <div className='profile' id="linkedin"> <a href="https://www.linkedin.com/in/mckennakblack" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn"></img></a></div>
+                    <div className='profile' id="instagram"> <a href="https://www.instagram.com/mckennakblack/" target="_blank" rel="noreferrer"><img src={instagram} alt="Instagram"></img></a></div>
+                    <div className='profile' id="email"> <a href="mailto:mckennakb@comcast.net?subject=Hello!"><img src={email} alt="Email Me"></img></a></div>
                 </div>
             </div>
             <footer>
