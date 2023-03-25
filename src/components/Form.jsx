8b1a72b4e978f1fdf3e30e117dd81ef63b15e4ac
McKB <mckennakb@comcast.net>
2023-03-25 18:05:55 -0400

    makes form work using emailJS

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
-const Form = (props) => {
+const Form = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault(); 
+        emailjs.sendForm('service_jj6t6my', 'template_aj73lzo', form.current, 'bZruj8ClypbV-8uwz')
+            .then((result) => {
+                console.log('success')
+            }, (error) => {
+                console.log('message failed to send')
+            });
+    }
     return (
-        <div>
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
                 <div>
-                    <input type="text" placeholder="Name" id="name" name="name" />
+                    <input type="text" placeholder="Name" id="name" name="name" required />
                 </div>
                 <div>
-                    <input type="email" id="email" placeholder="Email" name="email" />
+                    <input type="email" id="email" placeholder="Email" name="email" required />
                 </div>
                 <div>
-                    <textarea id="message" name="message" placeholder="Message" rows="5"></textarea>
+                    <textarea id="message" name="message" placeholder="Message" rows="5" required></textarea>
                 </div>
                 <div>
                     <button type="submit" id="send-button">Send</button>
                 </div>
             </form>
-        </div>
     )
 }
 
